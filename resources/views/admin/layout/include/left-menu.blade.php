5f9ba0f6a822f546d698e4115ba544217e54d987
Nguyen Tung Bach <bachntth2010055@fpt.edu.vn>
2021-10-11 12:26:08 +0700

    Template PHP

---
+++ resources/views/admin/layout/include/left-menu.blade.php
@@ -0,0 +1,40 @@
+<aside id="sidebar-left" class="sidebar-left">
+
+    <div class="sidebar-header">
+        <div class="sidebar-title" style="color: white">
+            Navigation
+        </div>
+        <div class="sidebar-toggle hidden-xs" data-toggle-class="sidebar-left-collapsed" data-target="html"
+             data-fire-event="sidebar-left-toggle">
+            <i class="fa fa-bars" aria-label="Toggle sidebar"></i>
+        </div>
+    </div>
+
+    <div class="nano">
+        <div class="nano-content">
+            <nav id="menu" class="nav-main" role="navigation">
+                <ul class="nav nav-main">
+                    <li class="nav-parent">
+                        <a class="bg-primary">
+                            <i class="fa fa-tasks" aria-hidden="true"></i>
+                            <span >Manage Product</span>
+                        </a>
+                        <ul class="nav nav-children">
+                            <li class="">
+                                <a href="/form">
+                                    Create Product
+                                </a>
+                            </li>
+                            <li class="">
+                                <a href="/table">
+                                    List Product
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+</aside>
